750d75666aa8b639be2de58386270fd944f29d88
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-09 19:18:05 -0400

    Created starting files for day 2

---
+++ Day2/js/script.js
@@ -0,0 +1,21 @@
+/*
+This is gonna be a multi lined comment
+Joshua Rosario
+7/9/15
+SDI Section 00
+Day 2 Coding Basics
+ */
+//alert("This is making sure our js file is connected");
+
+//This is a single line comment
+//Usually describes what comed right after
+
+/*
+multi line comment
+anything between the stars and slashes is ignored by the browsers
+ */
+
+//alert - gives the user info but returns nothing to us
+alert("Welcome to SDI");
+
+
