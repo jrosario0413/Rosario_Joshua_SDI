f47fa34576c9671f9411b0949cbbcc6f7b3e1724
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-11 18:03:10 -0400

    Basica Array
    Addition of array items

--- Day3/js/Day3_Arrays.js
+++ Day3/js/Day3_Arrays.js
@@ -7,3 +7,19 @@ Arrays
 
 //alert("mk");
 
+// Create a basic Array
+// Picking Oranges
+var orangeBins = [30,500,10002];
+
+// How many oranges did we pick?
+var totalOranges = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(totalOranges+" oranges were picked in 3 days");
+
+// Average number of oranges picked
+var averageOranges = totalOranges / 3;
+
+// Length property of an Arrays
+console.log(orangeBins.length);
+
+
+console.log("The average number of oranges picked is "+ averageOranges);
