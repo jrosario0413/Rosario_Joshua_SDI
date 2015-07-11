8106b6a4f35637126eac02f7af402a6235f35eb7
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-11 19:19:46 -0400

    Covered Prompts
    created area of rectangle

--- Day3/js/day3_props.js
+++ Day3/js/day3_props.js
@@ -5,4 +5,29 @@ Section 00
 Prompts
  */
 
-alert("test");
+//alert("test");
+
+// Ask the user for their input
+
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
+var age = 2015 - userInput;
+console.log("You are "+ age + " years old");
+
+// Calculate the area of a Rectangle
+
+//Prompt the user for width
+var width = prompt("Let's calculate the area of rectangle.\nPlease enter a width");
+
+//Prompt the user for height
+var height = prompt("Please enter the height");
+
+// Calculate the area using the user input
+var area = width * height;
+
+console.log("the area of your rectangle is "+area);
+
+// Create a variable that holds the whole result
+var result = "the area of your rectangle is " + area;
+console.log(result);
+alert(result);
