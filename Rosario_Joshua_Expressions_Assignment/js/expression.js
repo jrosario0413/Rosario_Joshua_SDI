247de56b029fec3f8152cd315f4338d1f3704873
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-11 22:37:30 -0400

    Calculate area and average of parallelogram
    first one done

--- Rosario_Joshua_Expressions_Assignment/js/expression.js
+++ Rosario_Joshua_Expressions_Assignment/js/expression.js
@@ -7,4 +7,16 @@ Expression Assignment
  */
 //alert("test");
 
- 
+ // I plan on coding how to calculate the area of 3 Parallelogram and find the average
+
+// ask the user for the measurements
+ alert("Calculate the area of 3 Parallelograms, and find the average area");
+
+var base1= prompt("What is the measurement of the 'base'?");
+ var height1 = prompt("What is the measurement of the 'height?");
+var area1 = base1 * height1;
+ console.log(area1);
+ alert("The area of the parallelogram is "+area1 + " square feet.");
+
+ // Calculated area of first parallelogram
+
