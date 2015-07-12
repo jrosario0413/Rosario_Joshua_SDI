cc1bf194fef062dd9e44a471d291938366253a27
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-11 22:48:35 -0400

    Calculated 2nd parallelogram

--- Rosario_Joshua_Expressions_Assignment/js/expression.js
+++ Rosario_Joshua_Expressions_Assignment/js/expression.js
@@ -12,11 +12,21 @@ Expression Assignment
 // ask the user for the measurements
  alert("Calculate the area of 3 Parallelograms, and find the average area");
 
-var base1= prompt("What is the measurement of the 'base'?");
- var height1 = prompt("What is the measurement of the 'height?");
+var base1= prompt("What is the measurement of the 'base' for first parallelogram?");
+ var height1 = prompt("What is the measurement of the 'height' for the first parallelogram?");
 var area1 = base1 * height1;
  console.log(area1);
  alert("The area of the parallelogram is "+area1 + " square feet.");
 
  // Calculated area of first parallelogram
 
+alert("Let's calculate area of the second parallelogram.");
+
+ //ask user for information
+ var base2 = prompt("What is the measurement of the 'base' for second parallelogram?");
+ var height2 = prompt("What is the measurement of the 'height' for the second parallelogram?");
+ var area2 = base2 * height2;
+ console.log(area2);
+ alert("The area of the parallelogram is "+area2 + " square feet.");
+
+ // Calculated area of second parallelogram
