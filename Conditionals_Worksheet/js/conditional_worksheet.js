f2ea9a694dc4c6581b6264ed3382b0e34de5a902
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-14 22:23:34 -0400

    Finished group 1

--- Conditionals_Worksheet/js/conditional_worksheet.js
+++ Conditionals_Worksheet/js/conditional_worksheet.js
@@ -12,3 +12,14 @@ Class: Conditional Worksheet
  // Last Chance for Gas
 
 
+var gasEfficiency = 20; //(in MPG's)
+ var gasReading = 50; // (in%)
+ var gasTank = 10; // (in gallons)
+
+ if(gasTank<gasReading){
+     console.log("You only have "+gasTank+" gallons of gas in your tank, better get gas now while you can!");
+ } else {
+     console.log("Yes, you can make it without stopping for gas!");
+ }
+
+ //
