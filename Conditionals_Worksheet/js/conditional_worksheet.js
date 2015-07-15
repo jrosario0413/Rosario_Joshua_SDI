a831e188db402b36f6ad71569faa01323b04181a
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-14 22:10:07 -0400

    Last Chance for Gas

--- Conditionals_Worksheet/js/conditional_worksheet.js
+++ Conditionals_Worksheet/js/conditional_worksheet.js
@@ -9,13 +9,6 @@ Class: Conditional Worksheet
 //alert("test");
 
 // Group 1: Expressions with Conditionals
- //Celsius to Fahrenheit Converter
+ // Last Chance for Gas
 
- var degrees1 =32;
- var degrees2 = 0;
- var degrees3 = 100;
- var degrees4 = 212;
- var degrees5 = 90;
- var degrees6 = 30.22;
 
- 
