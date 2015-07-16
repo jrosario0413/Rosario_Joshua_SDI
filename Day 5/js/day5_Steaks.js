3a7b1331562cc351d40b791b9ce3d82d916338ad
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-16 18:26:58 -0400

    Started day 5 steaks

---
+++ Day 5/js/day5_Steaks.js
@@ -0,0 +1,22 @@
+
+ /*
+Joshua Rosario
+Section 00
+Date: 7/16/15
+Class:
+ */
+
+//alert("test");
+
+ // Test a steak temperature to see if its done
+
+ /*
+  Extra-rare or Blue (bleu)	very red and cold	115–120 °F
+  Rare (saignant)	cold red center; soft	120–130 °F
+  Medium rare (à point)	warm red center; firmer	130–140 °F
+  Medium (demi-anglais)	pink and firm	140–150 °F	145 °F and rest for at least 3 minutes
+  Medium well (cuit)	small amount of pink in the center	150–160 °F
+  Well done (bien cuit)	gray-brown throughout; firm	160 °F+	160 °F for ground beef
+  */
+
+ 
