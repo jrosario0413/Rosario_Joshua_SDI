43788ba37f89d5f8f7954001202e150460f473a9
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-16 23:11:26 -0400

    Calculated Budget

--- Rosario_Joshua_Conditional_Assignment/js/conditionalAssignment.js
+++ Rosario_Joshua_Conditional_Assignment/js/conditionalAssignment.js
@@ -8,4 +8,35 @@ Class: Conditional Assignment
 
 //alert("test");
 
- 
+ // To calculate if you are a PC, Xbox, Playstation, or nintendo gamer
+ // PC gaming uses mouse and Keyboard - Precision - approximately $700
+ // Xbox - Halo/ Gears of War / TitanFall - bigger controller - approximately $350
+ // PlayStation - Uncharted/ Tomb Raider/ Bloodborne - smaller controller - approximately $400
+ // Nintendo ds - pokemon/ mario/ smash bros - approximately $160
+ // Why not enjoy all gaming systems - approximately $1600
+
+ //alert("What gaming console is for you?");
+//ask gamer for budget
+var gameBudget = prompt("First, What is your spending limit (Budget)?");
+console.log(gameBudget);
+
+ if(gameBudget<= 150){
+     alert("Save more money!");
+     console.log("Dude save your money!!!!!");
+ } else if(gameBudget <= 160){
+     alert("You can buy a Nintendo 3DS.");
+     console.log("You can buy a Nintendo 3DS.");
+ } else if(gameBudget >= 350&& gameBudget<=399){
+     alert("You can buy an Xbox One.");
+     console.log("You can buy an Xbox One.");
+ } else if (gameBudget >= 400&& gameBudget<=699){
+     alert("You can buy a PlayStation 4");
+     console.log("You can buy a PlayStation 4");
+ } else if(gameBudget>= 700){
+     alert("You can buy a PC");
+     console.log("You can buy a PC");
+ } else {
+     alert("You can actually buy all current gen consoles!!!!!!");
+     console.log("You can actually buy all current gen consoles!!!!!!");
+ }
+
