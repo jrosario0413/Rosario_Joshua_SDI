6adb9c7fb95550df470cf8c406e3339545c139bc
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-18 23:27:32 -0400

    Started and Finished do loops

---
+++ Loops_Worksheet/js/doWhileLoop.js
@@ -0,0 +1,21 @@
+
+ /*
+Joshua Rosario
+Section 00
+Date: 7/18/15
+Class:Do while loops
+ */
+
+//alert("test");
+
+ // Do while Loop
+
+ var kills = 0;
+
+ do{
+
+     console.log("You have "+kills+" kills");
+
+     kills++;
+ }while(kills<101);
+console.log("YOU ARE ON A 100 KILLING SPREE!!!!!");
