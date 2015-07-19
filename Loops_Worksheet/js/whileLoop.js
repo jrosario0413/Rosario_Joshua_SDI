483ec77ee986bbf1f76f28404d45de26b672850c
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-18 23:22:16 -0400

    While Loop Started and finished

---
+++ Loops_Worksheet/js/whileLoop.js
@@ -0,0 +1,21 @@
+
+ /*
+Joshua Rosario
+Section 00
+Date: 7/18/15
+Class:While Loop
+ */
+
+//alert("test");
+
+ // While Loop
+
+//Killstreaks
+
+ var kills =0;
+
+ while(kills<31){
+     console.log("You have "+kills+" kills.");
+     kills++
+ }
+ console.log("HOLY CRAP YOU HAVE A 30 KILLSTREAK!!!");
