45be19178427b4732cc9711a3784cabe23a969db
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-18 20:37:40 -0400

    end for loops

--- Day 6/js/day6ForLoops.js
+++ Day 6/js/day6ForLoops.js
@@ -7,3 +7,20 @@ Class: For Loops
  */
 
 //alert("test");
+
+ //Basic for loop
+ //for(initialize counter; condition; increment of change){}
+
+ for(var i = 10; i > 0; i--){
+     console.log("The value of i is "+i);
+ }
+
+ // Breaks - if this command is run, your loop will stop
+ for(var j = -10; j < 5; j--){
+     console.log("The value of j is "+j);
+
+     // IF the value of j is 3 stop my loop
+     if(j<-10000){
+         break;
+     }
+ }
