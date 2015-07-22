58744a991d7895445e25ee9b78f8d88783c261f5
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-21 23:01:02 -0400

    validate user

--- ArrayLoop_Worksheet/js/arrayLoop_worksheet.js
+++ ArrayLoop_Worksheet/js/arrayLoop_worksheet.js
@@ -12,21 +12,53 @@ Class: Array Loop Worksheet
  //Who leads the assault?
 
 // hero array
- var arr =["superman","batman","wonderwoman","flash","cyborg","aquaman","greenlantern"];
+ var arr =["superman","batman","flash","cyborg","aquaman"];
+ var heroFinder = false;
  //user chooses hero to lead assault
 
 
-var hero = prompt("Select your hero to lead the assault on Arkham Asylum\nsuperman, batman, wonderwoman, flash, cyborg, aquaman, greenlantern");
+var hero = prompt("Select your hero to lead the assault on Arkham Asylum\nsuperman, batman, flash, cyborg, aquaman");
+//validate user prompt
+ for(var j = 0; j < arr.length; j++){
+     if(hero === arr[j]){
+         heroFinder = true;
+     }
+ }
+
+ while(heroFinder === false){
+     hero = prompt("Please select a hero and do not leave blank")
+     j = 0;
+     for(var j = 0; j < arr.length; j++){
+         if(hero === arr[j]){
+             heroFinder = true;
+         }
+     }
+ }
 
 
-
-
- for(var i =0; i<arr.length; i++){
      //call out hero
-     if(arr[i]==="superman" || arr[i]==="batman"){
-         console.log("Good job "+arr[i]+". You lead assault.");
-     }else{
-         console.log("Follow the leader "+arr[i]);
+     if(hero==="superman") {
+         console.log("Good job "+hero+". You lead the assault!");
 
      }
+     else if(hero==="batman"){
+         console.log("Good job "+hero+". You lead the assault!");
+
+     }
+
+     else if (hero==="flash"){
+         console.log("Good job "+hero+". You lead the assault!");
+
+     }
+     else if(hero==="cyborg"){
+         console.log("Good job "+hero+". You lead the assault!");
+
+     }
+     else if(hero==="aquaman"){
+         console.log("Good job "+hero+". You lead the assault!");
+
+     }
+
+ for(var i =0; i<arr.length; i++) {
+    console.log("Follow the leader "+arr[i]);
  }
