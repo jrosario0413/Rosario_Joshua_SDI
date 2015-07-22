2e08ffa15b8199626a08aec3b0f4c8e0dface8a5
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-21 21:29:31 -0400

    created hero array

--- ArrayLoop_Worksheet/js/arrayLoop_worksheet.js
+++ ArrayLoop_Worksheet/js/arrayLoop_worksheet.js
@@ -8,3 +8,25 @@ Class: Array Loop Worksheet
 
 //alert("test");
 
+//Justice League Heroes
+ //Who leads the assault?
+
+// hero array
+ var arr =["superman","batman","wonderwoman","flash","cyborg","aquaman","greenlantern"];
+ //user chooses hero to lead assault
+
+
+var hero = prompt("Select your hero to lead the assault on Arkham Asylum\nsuperman, batman, wonderwoman, flash, cyborg, aquaman, greenlantern");
+
+
+
+
+ for(var i =0; i<arr.length; i++){
+     //call out hero
+     if(arr[i]==="superman" || arr[i]==="batman"){
+         console.log("Good job "+arr[i]+". You lead assault.");
+     }else{
+         console.log("Follow the leader "+arr[i]);
+
+     }
+ }
