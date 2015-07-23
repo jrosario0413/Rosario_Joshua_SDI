0d92ba9817019c90b0b06f0e8c813d227336fed3
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-23 17:56:07 -0400

    learned what functions are
    don’t forget function call

--- Day 8/js/day8Functions.js
+++ Day 8/js/day8Functions.js
@@ -6,4 +6,40 @@ Date:
 Class:
  */
 
-alert("test");
+//alert("test");
+
+ // Basic function structure
+ // function functionName(){//Code goes here}
+
+ //Create a function that will console.log something
+ function printHello(){
+     //This code will run when the function runs
+     console.log("This function is working.");
+ }
+
+ // Create a function call that will start our printHello function
+ //Go button
+ printHello();
+
+ printHello();
+
+
+ //Create a function that console logs a different string
+ function printMore(){
+     console.log("This is different text!");
+ }
+
+ // function call printMore
+ printMore();
+
+ //Create a function that calculates the area of a rectangle
+ function calcArea(){
+     // Create variables for width, height, and area
+     var width = 20;
+     var height= 30;
+     var area = width*height;
+     console.log("The area of the rectangle "+area);
+ }
+
+ //Create function call
+ calcArea();
