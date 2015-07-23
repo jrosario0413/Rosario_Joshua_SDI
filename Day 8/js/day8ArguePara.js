0c7ae2ac6c43eb539be62cb83d2bbba2cf298126
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-23 19:00:48 -0400

    old school way
    super specific function only will calculate the area of a rectangle 10
    by 20

---
+++ Day 8/js/day8ArguePara.js
@@ -0,0 +1,37 @@
+
+ /*
+Joshua Rosario
+Section 00
+Date: 
+Class:
+ */
+
+//alert("test");
+
+ // Arguements goes in the function call - "specific values"
+ //Parameters goes in the function definition and these catch the arguement - "Generic containers"
+
+
+ //Create variables for width and height
+ var width = 10;
+ var height = 20;
+
+
+
+ //Create a function to calculate the area of a rectangle
+
+ function calcArea(){
+
+     //Create variables
+     var width = 10;
+     var height = 20;
+
+     //Calculate area
+     var area = width*height;
+
+     //console log the area
+     console.log("The area of a rectangle with a width of "+width+" and a height of "+height+" is "+area);
+ }
+
+ //call the function
+ calcArea();
