1248667870c8caa33e9ab6f326f0188b333f5285
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-23 21:19:47 -0400

    created function for circle

--- Function Worksheet/js/functionWorksheet.js
+++ Function Worksheet/js/functionWorksheet.js
@@ -6,4 +6,29 @@ Date: 7/23/15
 Class:Function Worksheet
  */
 
-alert("test");
+//alert("test");
+
+ //Function worksheet
+
+ //Calculate the Circumference of a Circle
+ var radius = 5;
+ var diameter=5*2;
+ var pie = 3.14;
+ var circumference = pie*diameter;
+ console.log("The Circumference of the Circle is "+circumference);
+
+ //Create a function for circumference of a circle
+ function calcCircum(p,d){
+     // Calculate the circumference
+     var circumferenceTotal=p*d;
+
+     //Console.log the results
+     console.log("The circumference is "+circumferenceTotal);
+
+
+     //in order to use results in main code use return
+     //return - "Whatever you want to return";
+     return circumferenceTotal;
+
+ }
+
