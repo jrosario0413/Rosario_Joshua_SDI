c38a6f3c3d6465d6d29b2a8deafe2cbf5054289e
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-25 19:24:09 -0400

    Finished anonymous functions

--- Day 9/js/day9Ananymous.js
+++ Day 9/js/day9Ananymous.js
@@ -6,4 +6,23 @@ Date:
 Class:
  */
 
-alert("test");
+//alert("test");
+
+ //Basic Anonymous function
+ // var functionName = function(){};
+
+ //extremely popular in javascript and Jquery
+ //Important in object oriented programming
+ //For now, it is matter style choice between anom and regular function
+
+ //Create an anom function to calculate area of a triangle
+ var triArea = function (b,h) {
+     var area=.5*b*h;
+     return area;
+
+ };
+
+ //function call for anom function
+ var results = triArea(10,20);
+
+ console.log(results);
