5b8491d1c364c6160621316bb93c118097af2247
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-25 18:13:05 -0400

    completed function

--- Day 9/js/day9Random.js
+++ Day 9/js/day9Random.js
@@ -25,4 +25,45 @@ var min=prompt("We are going to find a random number between 2 values\nWhat is t
  //Number cast or parseInt
  min=Number(min);
 
- 
+ //variable for max
+ var max = prompt("What is the max value?");
+
+ while((isNaN(max)||max==="") || max<=min)
+     if(isNaN(max)){
+         max=prompt("Please only type in numbers.\nWhat is the max number?");
+     }else if(max<=min&& max!=""){
+         max=prompt("Please make sure that the max value is GREATER than the minimum value of "+min+".\nWhat is the max value?")
+     }else{
+         max=prompt("Please do not leave blank\nWhat is the max value?");
+     }
+
+ //Number Cast
+     max=Number(max);
+
+ //test values of min and max
+ console.log("The min value is "+min+".\nThe max value is "+max+".");
+
+
+ //Function call the randomizer function
+var results1= randomizer(min,max);
+console.log("Your random number is "+results1+".");
+
+
+ //15 random numbers to print the console.log
+ for(var i=0;i<15;i++){
+var tempNum = randomizer(min,max);
+     console.log(tempNum);
+ }
+
+
+
+
+ //Create the random function
+ function randomizer(mn,mx) {
+//generate our random number
+     var randomNum = Math.round(Math.random()*(mx-mn)+mn);
+console.log(randomNum);
+
+     //Return the random number to our main code
+     return randomNum;
+ }
