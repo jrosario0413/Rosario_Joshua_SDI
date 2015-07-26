568097fff1377283f2d0e246ada131fef2e1e014
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-25 22:29:41 -0400

    added comments and test results

--- Rosario_Joshua_Functions_Worksheet/js/functionWorksheet.js
+++ Rosario_Joshua_Functions_Worksheet/js/functionWorksheet.js
@@ -20,49 +20,62 @@ Class:
  var base = 5;
 var height = 6;
  //ASk user if they would like to calculate area of a parallelogram
- var userPrompt = prompt("Would you like to calculate the area of a parallelogram?\nPlease answer yes or no?");
+ var userPrompt = prompt("Would you like to calculate the area of a parallelogram and Circumference of a circle?\nPlease answer yes or no?");
 
  while(userPrompt.toLowerCase() !="yes" && userPrompt.toLowerCase()!="no"){
      userPrompt=prompt("Please only answer yes or no\nWould you like to calculate the area of a parallelogram?");
  }
 
- if(userPrompt.toLowerCase()==="yes"){
+ if(userPrompt.toLowerCase()==="yes") {
      //create a function to calculate area of a parallelogram
 
-     var paArea = function(b,h){
-         var area = b*h;
+     var paArea = function () {
+         var base = 5;
+         var height = 6;
+
+         var area = base * height;
          return area;
      };
 //function call to parallelogram area
-     var paAreaTotal = paArea(30,23);
-     console.log("The parallelogram area is "+paAreaTotal);
+     console.log(paArea(base,height));
+      console.log("The area is "+paArea(base,height)+" feet square.");
 
- }
-
-
-
+     var paAreaTotal = paArea();
 
 
      //create a function to calculate the circumference of a circle
-     var calcCircum = function (p, d) {
-         var circumTotal = p * d;
+     var calcCircum = function () {
+        var pie = Math.PI;
+         var diameter = 6;
+
+         var circumTotal = pie * diameter;
          return circumTotal;
      };
 
      //function call to Circumference
+     console.log(Math.round(calcCircum(pie,diameter)));
      var resultsCircum = calcCircum(pie, diameter);
-     console.log("The circumference is " + Math.round(resultsCircum));
-
-
- 
+     console.log("The circumference is " + Math.round(resultsCircum)+" feet.");
 
 
      //create a procedure that will console.log the circle circumference and area of parallelogram
      function printOut(c, a) {
-         console.log("The circumference of the circle is " + c);
-         console.log("The area of the parallelogram is " + a);
+         console.log("The circumference of the circle is " + c+" feet.");
+         console.log("The area of the parallelogram is " + a+" feet square");
      }
 
      //function call for printOut
+printOut(Math.round(resultsCircum),paAreaTotal);
 
-     printOut(Math.round(resultsCircum), paAreaTotal);
+ } else{
+     alert("Ok well see ya");
+ }
+
+ //Test codes
+ /*
+ I always tested the circumference with PI so only the diameter changed I started with 6 and total is 19 used 20 for diameter came out as 63
+ i also used 14 for diameter and total came out to 44
+
+ for the area of parallelogram i used base 5 and height 6 total was = 30
+ base = 5 height = 7 total = 35
+  */
