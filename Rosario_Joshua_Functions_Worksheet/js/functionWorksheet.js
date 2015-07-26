564974e110a10f2436d3d8684d70ef42d096f948
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-25 21:53:17 -0400

    Fixed function a bit

--- Rosario_Joshua_Functions_Worksheet/js/functionWorksheet.js
+++ Rosario_Joshua_Functions_Worksheet/js/functionWorksheet.js
@@ -34,37 +34,35 @@ var height = 6;
          return area;
      };
 //function call to parallelogram area
-     var paAreaTotal = paArea(base,height);
+     var paAreaTotal = paArea(30,23);
      console.log("The parallelogram area is "+paAreaTotal);
 
  }
 
 
 
- //create a function to calculate the circumference of a circle
- var calcCircum= function(p,d){
-     var circumTotal =p*d;
-     return circumTotal;
- };
-
- //function call to Circumference
- var resultsCircum = calcCircum(pie,diameter);
- console.log("The circumference is "+resultsCircum);
 
 
+     //create a function to calculate the circumference of a circle
+     var calcCircum = function (p, d) {
+         var circumTotal = p * d;
+         return circumTotal;
+     };
+
+     //function call to Circumference
+     var resultsCircum = calcCircum(pie, diameter);
+     console.log("The circumference is " + Math.round(resultsCircum));
 
 
- //function call
- var resultsCircum = calcCircum(pie,diameter);
- console.log("The Circumference is "+resultsCircum);
+ 
 
 
- //create a procedure that will console.log the circle circumference and area of parallelogram
- function printOut(c,a){
-     console.log("The circumference of the circle is "+c);
-     console.log("The area of the parallelogram is "+a);
- }
+     //create a procedure that will console.log the circle circumference and area of parallelogram
+     function printOut(c, a) {
+         console.log("The circumference of the circle is " + c);
+         console.log("The area of the parallelogram is " + a);
+     }
 
- //function call for printOut
+     //function call for printOut
 
- printOut(resultsCircum,paAreaTotal);
+     printOut(Math.round(resultsCircum), paAreaTotal);
