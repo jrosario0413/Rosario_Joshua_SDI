cdf7a01e8a6efc1d9566102f0f5f4d0c01bd9e70
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-25 20:00:30 -0400

    Learned if should use functions or not

--- Day 9/js/day9Multiple.js
+++ Day 9/js/day9Multiple.js
@@ -25,14 +25,14 @@ Class:Multple
 
  //function call to area
  var resultsArea = calcArea(height,width);
- console.log(resultsArea);
+ console.log("The area is "+resultsArea);
 
 
 
 
  //function call must go under function definition for an anonymous function
  var resultsPeri = calcPeri(height,width);
- console.log(resultsPeri);
+ console.log("The perimeter is "+resultsPeri);
 
  //create a function to calculate the area of a rectangle
  function calcArea(h,w){
@@ -40,15 +40,33 @@ Class:Multple
      return area;
  }
 
-//create a function to calculate area of a triangle
- var triArea = function(b,h){
-     var area = .5*b*h;
-     return area;
- };
+ //ASk user if they would like to calculate area of a triangle
+ var userPrompt = prompt("Would you like to calculate the area of a rectangle?\nPlease answer yes or no?");
 
- //function call to triangle area
- var triAreaTotal = triArea(base,height);
- console.log(triAreaTotal);
+ while(userPrompt.toLowerCase() !="yes" && userPrompt.toLowerCase()!="no"){
+     userPrompt=prompt("Please only answer yes or no\nWould you like to calculate the area of a triangle?");
+ }
+
+ //conditional to see if we should run the anonymous function
+ if(userPrompt.toLowerCase()==="yes"){
+     //create a function to calculate area of a triangle
+
+     var triArea = function(b,h){
+         var area = .5*b*h;
+         return area;
+     };
+
+     //function call to triangle area
+     var triAreaTotal = triArea(base,height);
+     console.log("The Triangle area is "+triAreaTotal);
+ }
+
+
+
+
+
+
+
 
  //create a procedure that will console.log the rectangle perimeter and area
  function printOut(p,a){
