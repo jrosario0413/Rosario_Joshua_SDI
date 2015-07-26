ed069cd1946ef43f818132736a39bb6ccb275fc0
jrosario0413 <jlrosarioespinos@fullsail.edu>
2015-07-25 21:23:34 -0400

    Function for circumference

--- Rosario_Joshua_Functions_Worksheet/js/functionWorksheet.js
+++ Rosario_Joshua_Functions_Worksheet/js/functionWorksheet.js
@@ -7,5 +7,38 @@ Class:
  */
 
 //alert("test");
+//Function Worksheet
 
- 
+ //Find circumference of a Circle and Area
+
+ //Create variables for height and width
+ var pie = 3.14;
+ var diameter = 6;
+
+
+ //create  another variable for a base of a triangle
+ var base = 5;
+
+
+ //create a function to calculate the circumference of a circle
+ var calcCircum= function(p,d){
+     var circumTotal =p*d;
+     return circumTotal;
+ };
+
+ //function call to Circumference
+ var resultsCircum = calcCircum(pie,diameter);
+ console.log("The circumference is "+resultsCircum);
+
+
+
+
+ //function call
+ var resultsCircum = calcCircum(pie,width);
+ console.log("The perimeter is "+resultsPeri);
+
+ //create a function to calculate the area of a rectangle
+ function calcArea(h,w){
+     var area = w*h;
+     return area;
+ }
